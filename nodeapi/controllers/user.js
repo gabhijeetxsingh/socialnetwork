060295a85baad5e7c2abcaf9b9581de551ecd5c5
gabhijeetxsingh <abhijeetvns03@gmail.com>
2019-10-30 23:16:52 +0530

    update user using put

--- nodeapi/controllers/user.js
+++ nodeapi/controllers/user.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const User = require("../models/user");
 
 exports.userById = (req, res, next, id) => {
@@ -41,4 +42,23 @@ exports.getUser = (req, res, next) => {
 	req.profile.hashed_password = undefined;
 	req.profile.salt = undefined;
 	return res.json(req.profile)
+}
+
+exports.updateUser = (req, res, next) => {
+
+	let user = req.profile;
+	user = _.extend(user, req.body); // extend will mutate the source object
+	user.updated = Date.now();
+	user.save((err) => {
+		if(err) {
+			return res.status(400).json({
+				error : "You are not authorized to perform this action"
+			})
+		}
+		user.hashed_password = undefined;
+		user.salt = undefined;
+		res.json({
+			user
+		})
+	});
 }
--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {userById, allUsers, getUser} = require("../controllers/user");
+const {userById, allUsers, getUser, updateUser} = require("../controllers/user");
 const {requireSignin} = require("../controllers/auth");
 
 
 router.get('/users', allUsers);
 router.get('/user/:userId', requireSignin, getUser);
+router.put('/user/:userId', requireSignin, updateUser);
 
 //any route containing :userId, our app will first execute userById()
 router.param("userId", userById);
