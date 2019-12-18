cfaff7d0a090faa274af486419a387ea78078da4
gabhijeetxsingh <abhijeetvns03@gmail.com>
2019-12-18 13:50:38 +0530

    updated title

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -1,117 +1,162 @@
 const Post = require("../models/post");
 const formidable = require('formidable');
 const fs = require("fs")
+const _ = require("lodash");
 
-exports.postById = async (req , res, next, id) => {
+exports.postById = async (req, res, next, id) => {
 
-	try {
+    try {
 
-		Post.findById(id)
-		.populate("postedBy", "_id name")
-		.exec((err, post) => {
-			if(err || !post) {
+        Post.findById(id)
+            .populate("postedBy", "_id name")
+            .exec((err, post) => {
+                if (err || !post) {
 
-				return res.status(400).json({
-					error : err
-				})
+                    return res.status(400).json({
+                        error: err
+                    })
 
-			}
+                }
 
-			req.post = post;
-			next();
-		})
-	}
-	catch(err) {
-		console.log(err);
-	}
+                req.post = post;
+                next();
+            })
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-exports.getPosts = async (req , res) => {
+exports.getPosts = async (req, res) => {
 
-	try {
+    try {
 
-		const posts = await Post.find().populate("postedBy", "_id name").select("_id title body");
-		res.json({posts});
-	}
-	catch(err) {
-		console.log(err);
-	}
+        const posts = await Post.find().populate("postedBy", "_id name").select("_id title body");
+        res.json({ posts });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 exports.createPost = (req, res) => {
-	console.log(req.body)
-	let form = new formidable.IncomingForm();
-	form.keepExtensions = true;
-	form.parse(req, (err, fields, files)=>{
-		if(err) {
-			return res.status(400).json({
-				error : "Image could not be uploaded"
-			})
-		}
-		let post = new Post(fields);
+    console.log(req.body)
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Image could not be uploaded"
+            })
+        }
+        let post = new Post(fields);
 
         req.profile.hashed_password = undefined;
         req.profile.salt = undefined;
         post.postedBy = req.profile;
 
-		if(files.photo) {
-			post.photo.data = fs.readFileSync(files.photo.path);
-			post.photo.contentType = files.photo.type;
-		}
+        if (files.photo) {
+            post.photo.data = fs.readFileSync(files.photo.path);
+            post.photo.contentType = files.photo.type;
+        }
 
-		post.save((err, result)=>{
-			if(err) {
-				return res.status(400).json({
-					error : err
-				})
-			}
-			
-			res.json(result)
-			
-		})
-	})
+        post.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                })
+            }
+
+            res.json(result)
+
+        })
+    })
 
 }
 
 exports.postsByUser = (req, res) => {
-	Post.find({postedBy : req.profile._id}).populate('postedBy',"_id name")
-	.select('_id title body')
-	.sort("_created")
-	.exec((err, posts)=>{
-		if(err) {
-			return res.status(400).json({
-				error : err
-			})
-		}
-		res.json(posts);
-	})
+    Post.find({ postedBy: req.profile._id }).populate('postedBy', "_id name")
+        .select('_id title body')
+        .sort("_created")
+        .exec((err, posts) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                })
+            }
+            res.json(posts);
+        })
 }
 
 exports.isPoster = (req, res, next) => {
-	let isPoster = req.post && req.auth && req.post.postedBy._id == req.auth._id;
+    let isPoster = req.post && req.auth && req.post.postedBy._id == req.auth._id;
 
-	if(!isPoster) {
-		return res.status(400).json({
-			error : "User is not Authorised"
-		})	
-	}
+    if (!isPoster) {
+        return res.status(400).json({
+            error: "User is not Authorised"
+        })
+    }
 
-	next();
+    next();
 }
+
+exports.updatePost = (req, res, next) => {
+    let post = req.post;
+    post = _.extend(post, req.body);
+    post.updated = Date.now();
+    post.save((err, post) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            })
+        }
+        
+        res.json(post)
+    })
+}
+
+// exports.updatePost = (req, res, next) => {
+//     let form = new formidable.IncomingForm();
+//     form.keepExtensions = true;
+//     form.parse(req, (err, fields, files) => {
+//         if (err) {
+//             return res.status(400).json({
+//                 error: 'Photo could not be uploaded'
+//             });
+//         }
+//         // save post
+//         let post = req.post;
+//         post = _.extend(post, fields);
+//         post.updated = Date.now();
+
+//         if (files.photo) {
+//             post.photo.data = fs.readFileSync(files.photo.path);
+//             post.photo.contentType = files.photo.type;
+//         }
+
+//         post.save((err, result) => {
+//             if (err) {
+//                 return res.status(400).json({
+//                     error: err
+//                 });
+//             }
+//             res.json(post);
+//         });
+//     });
+// };
+
+
 
 exports.deletePost = (req, res, next) => {
-	let post = req.post;
+    let post = req.post;
 
-	post.remove((err, post)=>{
-		if(err) {
-			return res.status(400).json({
-				error : err
-			})
-		}
+    post.remove((err, post) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            })
+        }
 
-		res.json({
-			message : "Post deleted Successfully"
-		})
-	})
+        res.json({
+            message: "Post deleted Successfully"
+        })
+    })
 }
-
--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getPosts, createPost, postsByUser, postById, isPoster, deletePost } = require("../controllers/post");
+const { getPosts, createPost, postsByUser, postById, isPoster, deletePost, updatePost } = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 const { createPostValidator } = require("../validator");
@@ -9,6 +9,7 @@ const { createPostValidator } = require("../validator");
 router.get('/', getPosts);
 router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
 router.get('/posts/by/:userId', requireSignin, postsByUser);
+router.put('/post/:postId', requireSignin, isPoster, updatePost);
 router.delete('/post/:postId', requireSignin, isPoster, deletePost);
 
 //any route containing :userId, our app will first execute userById()
