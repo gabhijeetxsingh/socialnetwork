cd1ea308e4cf3b894d04766a7f9fea7afaa9232d
gabhijeetxsingh <abhijeetvns03@gmail.com>
2019-12-18 14:54:42 +0530

    added cors

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
 const mongoose = require("mongoose");
+const fs = require("fs");
+const cors = require("cors");
 const expressValidator = require("express-validator");
 const app = express();
 
@@ -21,11 +23,26 @@ const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 
+//api docs
+app.get("/", (req, res) => {
+	fs.readFile('docs/apiDocs.json',(err, data ) => {
+		if(err) {
+			res.status(400).json({
+				error : err
+			})
+		}
+
+		const docs = JSON.parse(data);
+		res.json(docs);
+	})
+})
+
 //middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 app.use("/", postRoutes);
 app.use("/", authRoutes);
