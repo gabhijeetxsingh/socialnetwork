19f3b22bdcdc4f2522f69a2c2540812e74dd02ff
gabhijeetxsingh <abhijeetvns03@gmail.com>
2019-12-18 21:41:23 +0530

    added react frontend

---
+++ react-front/src/App.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <h1>React Frontend</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
