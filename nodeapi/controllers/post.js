abe82e6f42c4681a288ace9032b21eb883e239ca
gabhijeetxsingh <abhijeetvns03@gmail.com>
2019-12-18 11:28:01 +0530

    getting all posts by users

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -6,7 +6,7 @@ exports.getPosts = async (req , res) => {
 
 	try {
 
-		const posts = await Post.find().select("_id title body");
+		const posts = await Post.find().populate("postedBy", "_id name").select("_id title body");
 		res.json({posts});
 	}
 	catch(err) {
@@ -15,6 +15,7 @@ exports.getPosts = async (req , res) => {
 }
 
 exports.createPost = (req, res) => {
+	console.log(req.body)
 	let form = new formidable.IncomingForm();
 	form.keepExtensions = true;
 	form.parse(req, (err, fields, files)=>{
@@ -23,10 +24,12 @@ exports.createPost = (req, res) => {
 				error : "Image could not be uploaded"
 			})
 		}
-		
 		let post = new Post(fields);
 
-		post.postedby = req.profile;
+        req.profile.hashed_password = undefined;
+        req.profile.salt = undefined;
+        post.postedBy = req.profile;
+
 		if(files.photo) {
 			post.photo.data = fs.readFileSync(files.photo.path);
 			post.photo.contentType = files.photo.type;
@@ -37,10 +40,25 @@ exports.createPost = (req, res) => {
 				return res.status(400).json({
 					error : err
 				})
-
-				res.json(result)
 			}
+			
+			res.json(result)
+			
 		})
 	})
 
+}
+
+exports.postsByUser = (req, res) => {
+	Post.find({postedBy : req.profile._id}).populate('postedBy',"_id name")
+	.select('_id title body')
+	.sort("_created")
+	.exec((err, posts)=>{
+		if(err) {
+			return res.status(400).json({
+				error : err
+			})
+		}
+		res.json(posts);
+	})
 }
--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const {getPosts, createPost} = require("../controllers/post");
+const {getPosts, createPost, postsByUser} = require("../controllers/post");
 const {requireSignin} = require("../controllers/auth");
 const {userById} = require("../controllers/user");
 const {createPostValidator} = require("../validator");
 
 
 router.get('/', getPosts);
-router.post('/post', requireSignin, createPostValidator, createPost);
+router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
+router.get('/posts/by/:userId', requireSignin, postsByUser);
 
 //any route containing :userId, our app will first execute userById()
 router.param("userId", userById);
